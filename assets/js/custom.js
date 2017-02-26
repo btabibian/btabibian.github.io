6dbd8f9e42cbd1900b28fa7b46bb08bce1dc38a8
Behzad Tabibian <btabibian@gmail.com>
2017-02-26 12:09:55 +0100

    new quote

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,4 +5,14 @@ $(function() {
 function hide_image() {
     $("#main_imagediv").fadeOut(10);
     $("#qoutediv").fadeIn(200);
+
+}
+
+$(function() {
+    $("#qoutediv_2").hide();
+});
+
+function hide_image_2() {
+    $("#main_imagediv_2").fadeOut(10);
+    $("#qoutediv_2").fadeIn(200);
 }
